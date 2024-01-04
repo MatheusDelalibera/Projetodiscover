8ad33000c39a2e2a6dd1823f88810368ceac5a33
MatheusDelalibera <matheus.delalibera13@icloud.com>
2024-01-04 08:36:37 -0300

    mudando para perfil da namorada

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ const img=document.querySelector('profile img')
     // substituir a imagem
 if(html.classList.contains('light')) {
    // se tiver light mode, adicionar a imagem light 
-img.setAttribute('src', './assets/avatar-light.png') 
+img.setAttribute('src', './assets/profile.png') 
 } else {
     // se tiver sem light mode, manter a imagem normal
-img.setAttribute('src', './assets/avatar.png')
+img.setAttribute('src', './assets/profile-night.png')
 }
 
 
